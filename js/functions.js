eac32e872ffffce3d22df322fcb38a495d126a1b
Balkhanakovv <balkhanakov.vl@gmail.com>
2021-07-04 19:13:34 +0700

    add brightness and contrast; add sepia filter; add controls for canny; add blur

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,14 @@
+function doubleRound (number)
+{
+     if (number < 0)
+          number = 0.0;
+
+     if (number > 255)
+          number = 255.0;
+
+     return number;
+}
+
 function convertRGBToGray(src) {
      var dst = new cv.Mat();
      cv.cvtColor(src, dst, cv.COLOR_RGB2GRAY, 0);
@@ -11,7 +22,37 @@ function cannyFilter(src, thresgoldMin = 50, thresholdMax = 100, apertureSize = 
      return dst;
 }
 
-function sepia(src) {
-    
+function blur(src, blurSize = 5) {
+     let dst = new cv.Mat();
+     cv.medianBlur(src, dst, blurSize);
+     return dst;
+}
+
+function sepiaFilter(src) {
+     let dst = src.clone();
+
+     for (var i = 0; i < dst.cols; i++)
+          for (var j = 0; j < dst.rows; j++) {
+               
+               dst.ucharPtr(i, j)[0] = doubleRound(dst.ucharPtr(i, j)[2] * (272 / 1000) + dst.ucharPtr(i, j)[1] * (534 / 1000) + dst.ucharPtr(i, j)[0] * (131 / 1000));
+               dst.ucharPtr(i, j)[1] = doubleRound(dst.ucharPtr(i, j)[2] * (349 / 1000) + dst.ucharPtr(i, j)[1] * (686 / 1000) + dst.ucharPtr(i, j)[0] * (168 / 1000));
+               dst.ucharPtr(i, j)[2] = doubleRound(dst.ucharPtr(i, j)[2] * (393 / 1000) + dst.ucharPtr(i, j)[1] * (769 / 1000) + dst.ucharPtr(i, j)[0] * (189 / 1000));
+               
+          }
+
+     return dst;
+}
+
+function brightnessAndContrast (src, brightness, contrast) {
+     let dst = src.clone();
+
+     for (var i = 0; i < dst.cols; i++)
+          for (var j = 0; j < dst.rows; j++) {
+               dst.ucharPtr(i, j)[0] = doubleRound(dst.ucharPtr(i, j)[0] * contrast * 0.1 + brightness);
+               dst.ucharPtr(i, j)[1] = doubleRound(dst.ucharPtr(i, j)[1] * contrast * 0.1 + brightness);
+               dst.ucharPtr(i, j)[2] = doubleRound(dst.ucharPtr(i, j)[2] * contrast * 0.1 + brightness);
+          }
+
+     return dst;
 }
 
--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ var dstImg = null;
 let imgElement = document.getElementById('imgInput');
 let inputElement = document.getElementById('fileInput');
 
+document.getElementById('cannyControls').style.visibility = "hidden";
+document.getElementById('blurControl').style.visibility = "hidden";
+document.getElementById('bcControls').style.visibility = "hidden";
+
 inputElement.addEventListener('change', (e) => {
      srcImg = URL.createObjectURL(e.target.files[0]);
      imgElement.src = srcImg;
@@ -16,6 +20,9 @@ function onOpenCvReady() {
 }
 
 function bwEvent() {
+     document.getElementById('cannyControls').style.visibility = "hidden";
+     document.getElementById('blurControl').style.visibility = "hidden";
+     document.getElementById('bcControls').style.visibility = "hidden";
      if (srcImg == null) return;
 
      let src = cv.imread(imgElement);
@@ -27,10 +34,57 @@ function bwEvent() {
 }
 
 function cannyEvent() {
+     document.getElementById('cannyControls').style.visibility = "visible";
+     document.getElementById('blurControl').style.visibility = "hidden";
+     document.getElementById('bcControls').style.visibility = "hidden";
      if (srcImg == null) return;
 
      let src = cv.imread(imgElement);
-     dstImg = cannyFilter(src);
+     dstImg = cannyFilter(src, document.getElementById('cannyTrackbarMin').value * 1.0, document.getElementById('cannyTrackbarMax').value * 1.0);
+
+     cv.imshow('canvasOutput', dstImg);
+
+     src.delete();
+}
+
+function blurEvent() {
+     document.getElementById('cannyControls').style.visibility = "hidden";
+     document.getElementById('blurControl').style.visibility = "visible";
+     document.getElementById('bcControls').style.visibility = "hidden";
+     if (srcImg == null) return;
+
+     let src = cv.imread(imgElement);
+     dstImg = blur(src, document.getElementById('blurSize').value * 1.0);
+
+     cv.imshow('canvasOutput', dstImg);
+
+     src.delete();
+}
+
+function sepiaEvent(src) {
+     document.getElementById('cannyControls').style.visibility = "hidden";
+     document.getElementById('blurControl').style.visibility = "hidden";
+     document.getElementById('bcControls').style.visibility = "hidden";
+     if (srcImg == null) return;
+
+     var src = cv.imread(imgElement);
+     
+     dstImg = sepiaFilter(src);
+
+     cv.imshow('canvasOutput', dstImg);
+
+     src.delete();
+}
+
+function bcEvent() {
+     document.getElementById('cannyControls').style.visibility = "hidden";
+     document.getElementById('blurControl').style.visibility = "hidden";
+     document.getElementById('bcControls').style.visibility = "visible";
+     if (srcImg == null) return;
+
+     var src = cv.imread(imgElement);
+
+     dstImg = brightnessAndContrast(src, document.getElementById('brightnessTrackbar').value * 1.0, document.getElementById('contrastTrackBar').value * 1.0);
 
      cv.imshow('canvasOutput', dstImg);
 
