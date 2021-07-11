2ae2e85b81bdc94906c431c6d3d56c34473126f1
Balkhanakovv <balkhanakov.vl@gmail.com>
2021-07-11 18:44:17 +0700

    fixed image save (save image with real size)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-var srcImg = null;
-var dstImg = null;
+let srcImg = null;
+let dstImg = null;
+let Img = null;
 
 let imgElement = document.getElementById('canvasInput');
 let inputElement = document.getElementById('fileInput');
 let applyButton = document.getElementById('ApplyButton');
 let saveButton = document.getElementById('SaveButton');
 let canvasOutput = document.getElementById('canvasOutput');
+let imageHidden = document.getElementById('imageHidden');
+let canvasHidden = document.getElementById('hiddenCanvasOutput');
 
 document.getElementById('cannyControls').style.visibility = "hidden";
 document.getElementById('blurControl').style.visibility = "hidden";
@@ -22,6 +25,7 @@ inputElement.addEventListener('change', (e) => {
      srcImg = URL.createObjectURL(e.target.files[0]);
      
      imgElement.src = srcImg;
+     imageHidden.src = srcImg;
 
      dstImg = new cv.Mat();
 
@@ -44,7 +48,7 @@ applyButton.addEventListener('click', (e) => {
 
 saveButton.addEventListener('click', (e) => {
      if (dstImg != null) {
-          var tmpImgURL = canvasOutput.toDataURL();
+          var tmpImgURL = hiddenCanvasOutput.toDataURL();
 
           var newTab = window.open('about:blank','image from canvas');
           newTab.document.write("<img src='" + tmpImgURL + "' alt='from canvas'/>"); 
@@ -70,11 +74,15 @@ function bwEvent() {
      if (srcImg == null) return;
 
      let src = cv.imread(imgElement);
+     let srcFull = cv.imread(imageHidden);
 
      dstImg = convertRGBToGray(src);
+     Img = convertRGBToGray(srcFull);
      cv.imshow('canvasOutput', dstImg);
+     cv.imshow('hiddenCanvasOutput', Img);
 
      src.delete();
+     srcFull.delete();
 }
 
 function cannyEvent() {
@@ -84,11 +92,15 @@ function cannyEvent() {
      if (srcImg == null) return;
 
      let src = cv.imread(imgElement);
+     let srcFull = cv.imread(imageHidden);
 
      dstImg = cannyFilter(src, document.getElementById('cannyTrackbarMin').value * 1.0, document.getElementById('cannyTrackbarMax').value * 1.0);
+     Img = cannyFilter(srcFull, document.getElementById('cannyTrackbarMin').value * 1.0, document.getElementById('cannyTrackbarMax').value * 1.0);
      cv.imshow('canvasOutput', dstImg);
+     cv.imshow('hiddenCanvasOutput', Img);
 
      src.delete();
+     srcFull.delete();
 }
 
 function blurEvent() {
@@ -98,11 +110,15 @@ function blurEvent() {
      if (srcImg == null) return;
 
      let src = cv.imread(imgElement);
+     let srcFull = cv.imread(imageHidden);
 
      dstImg = blur(src, document.getElementById('blurSize').value * 1.0);
+     Img = blur(srcFull, document.getElementById('blurSize').value * 1.0);
      cv.imshow('canvasOutput', dstImg);
+     cv.imshow('hiddenCanvasOutput', Img);
 
      src.delete();
+     srcFull.delete();
 }
 
 function sepiaEvent() {
@@ -111,12 +127,16 @@ function sepiaEvent() {
      document.getElementById('bcControls').style.visibility = "hidden";
      if (srcImg == null) return;
 
-     var src = cv.imread(imgElement);
+     let src = cv.imread(imgElement);
+     let srcFull = cv.imread(imageHidden);
      
      dstImg = sepiaFilter(src);
+     Img = sepiaFilter(srcFull);
      cv.imshow('canvasOutput', dstImg);
+     cv.imshow('hiddenCanvasOutput', Img);
 
      src.delete();
+     srcFull.delete();
 }
 
 function bcEvent() {
@@ -125,12 +145,16 @@ function bcEvent() {
      document.getElementById('bcControls').style.visibility = "visible";
      if (srcImg == null) return;
 
-     var src = cv.imread(imgElement);
+     let src = cv.imread(imgElement);
+     let srcFull = cv.imread(imageHidden);
 
      dstImg = brightnessAndContrast(src, document.getElementById('brightnessTrackbar').value * 1.0, document.getElementById('contrastTrackBar').value * 1.0);
+     Img = brightnessAndContrast(srcFull, document.getElementById('brightnessTrackbar').value * 1.0, document.getElementById('contrastTrackBar').value * 1.0);
      cv.imshow('canvasOutput', dstImg);
+     cv.imshow('hiddenCanvasOutput', Img);
 
      src.delete();
+     srcFull.delete();
 }
 
 function cartoonEvent() {
@@ -139,10 +163,14 @@ function cartoonEvent() {
      document.getElementById('bcControls').style.visibility = "hidden";
      if (srcImg == null) return;
 
-     var src = cv.imread(imgElement);
+     let src = cv.imread(imgElement);
+     let srcFull = cv.imread(imageHidden);
 
      dstImg = cartoonFilter(src);
+     Img = cartoonFilter(srcFull);
      cv.imshow('canvasOutput', dstImg);
+     cv.imshow('hiddenCanvasOutput', Img);
 
      src.delete();
+     srcFull.delete();
 }
